f38caebdf10b8a3b4a07c5b43bf5f2336c4fc779
Justin Furuness <jfuruness@gmail.com>
2023-12-04 02:50:55 -0500

    added retries

--- mrt_collector/mrt_file.py
+++ mrt_collector/mrt_file.py
@@ -5,6 +5,7 @@ from urllib.parse import quote
 import warnings
 
 import requests
+from requests.adapters import HTTPAdapter, Retry
 
 from .sources import Source
 
@@ -30,16 +31,24 @@ class MRTFile:
         """Downloads the raw file if you haven't already"""
 
         if not self.downloaded:
-            # https://stackoverflow.com/a/39217788/8903959
-            # stream works best for large files
-            # https://docs.python.org/3.5/library/urllib.request.html#legacy-interface
-            with requests.get(self.url, stream=True, timeout=60) as r:  # verify=verify
-                if r.status_code == 200:
-                    with self.raw_path.open("wb") as f:
-                        shutil.copyfileobj(r.raw, f)  # type: ignore
-                # Don't error, some files always fail to download
-                else:
-                    warnings.warn(f"status of {r.status_code} for {self.url}")
+            # https://stackoverflow.com/a/35504626/8903959
+            with requests.Session() as session:
+                retries = Retry(total=5,
+                                backoff_factor=0.1,
+                                status_forcelist=[ 500, 502, 503, 504 ])
+
+                session.mount('http://', HTTPAdapter(max_retries=retries))
+                session.mount('https://', HTTPAdapter(max_retries=retries))
+
+                # https://stackoverflow.com/a/39217788/8903959
+                # stream works best for large files
+                with session.get(self.url, stream=True, timeout=60) as r:
+                    if r.status_code == 200:
+                        with self.raw_path.open("wb") as f:
+                            shutil.copyfileobj(r.raw, f)  # type: ignore
+                    # Don't error, some files always fail to download
+                    else:
+                        warnings.warn(f"status of {r.status_code} for {self.url}")
 
     def _url_to_fname(self, url: str, ext: str = "") -> str:
         """Converts a URL into a file name"""
