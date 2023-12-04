61c144cb4bfd26b688e301ad952117d68e05983f
Justin Furuness <jfuruness@gmail.com>
2023-12-04 04:15:01 -0500

    updated expecting time to download MRTs

--- mrt_collector/mrt_collector.py
+++ mrt_collector/mrt_collector.py
@@ -83,7 +83,7 @@ class MRTCollector:
                 for future in tqdm(
                     as_completed(futures),
                     total=len(mrt_files),
-                    desc="Downloading MRTs (~1hr)",
+                    desc="Downloading MRTs (~12m)",
                 ):
                     # reraise any exceptions from the threads
                     future.result()
