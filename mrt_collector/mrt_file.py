42b5ee4f50dbc9d06010b980935609d10a25d4bd
Justin Furuness <jfuruness@gmail.com>
2023-12-04 03:50:25 -0500

    Added retry mechanism

--- mrt_collector/mrt_file.py
+++ mrt_collector/mrt_file.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import os
 import shutil
+import time
 from urllib.parse import quote
 import warnings
 
 import requests
-from requests.adapters import HTTPAdapter, Retry
 
 from .sources import Source
 
@@ -27,28 +27,36 @@ class MRTFile:
         # self.prefixes_path: Path = prefixes_dir
         # self.formatted_path: Path = formatted_dir
 
-    def download_raw(self) -> None:
+    def download_raw(self, retries: int = 3) -> None:
         """Downloads the raw file if you haven't already"""
 
-        if not self.downloaded:
-            # https://stackoverflow.com/a/35504626/8903959
-            with requests.Session() as session:
-                retries = Retry(total=5,
-                                backoff_factor=0.1,
-                                status_forcelist=[ 500, 502, 503, 504 ])
+        if self.downloaded:
+            return
 
-                session.mount('http://', HTTPAdapter(max_retries=retries))
-                session.mount('https://', HTTPAdapter(max_retries=retries))
-
-                # https://stackoverflow.com/a/39217788/8903959
-                # stream works best for large files
-                with session.get(self.url, stream=True, timeout=60) as r:
+        # I tried using proper backoff strategies, such as:
+        # https://stackoverflow.com/a/35504626/8903959
+        # But this actually doesn't capture incomplete read
+        # errors in URL lib. So I need to write my own.
+        status_code = 0
+        for i in range(retries):
+            try:
+                with requests.get(self.url, stream=True, timeout=60) as r:
+                    status_code = r.status_code
                     if r.status_code == 200:
                         with self.raw_path.open("wb") as f:
                             shutil.copyfileobj(r.raw, f)  # type: ignore
-                    # Don't error, some files always fail to download
-                    else:
-                        warnings.warn(f"status of {r.status_code} for {self.url}")
+                            return
+            except Exception as e:
+                print(f"URL {self.url} failed due to {e} {type(e)} {i + 1}/{retries}")
+                if i == retries - 1:
+                    raise
+
+            time.sleep((i + 1) * 10)
+
+        # Don't error, sometiems files fail due to not found errors (404)
+        warnings.warn(
+            f"status of {status_code} for {self.url}, download failed but didn't err"
+        )
 
     def _url_to_fname(self, url: str, ext: str = "") -> str:
         """Converts a URL into a file name"""
