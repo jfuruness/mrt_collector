5479eafab24cd78b4be750649483c795f6c5ad78
Justin Furuness <jfuruness@gmail.com>
2023-12-06 18:08:30 -0500

    removed useless scripts

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-path = Path("/home/anon/mrt_data/2023-11-01/parsed/")
-path = path / "non_urlhttp%3A__data.ris.ripe.net_rrc25__2023.11_bview.20231101.json"
-
-with path.open() as f:
-    data = json.load(f)
-    print(len(data))
