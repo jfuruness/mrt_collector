601c5b4136f60c404ac2d37293ab4109b70df2b2
Diaz, Satchel <satchel.diaz@uconn.edu>
2026-02-03 21:59:26 -0500

    Added optional CMD arg to limit amt of files to process, passes into MRTCollector __init__

--- mrt_collector/__main__.py
+++ mrt_collector/__main__.py
@@ -5,15 +5,25 @@ from pathlib import Path
 from .analyzers import MHExportAnalyzer
 from .mrt_collector import MRTCollector
 
+import argparse
 
 def main():
     print("HERE")
+
+    parser = argparse.ArgumentParser(prog='MRT Collector')
+
+    parser.add_argument('-lf', '--limit_files', type=int, help='Number of files to process; Leave blank for all')
+
+    args = parser.parse_args()
+    limit_files_to = 0 if args.limit_files is None else args.limit_files
     # dl_time=datetime(2025, 3, 20, 0, 0, 0)
-    # collector = MRTCollector(
-    #     dl_time=dl_time,
-    #     cpus=cpu_count(),
-    #     base_dir=Path.home() / "mrt_data" / dl_time.strftime("%Y_%m_%d")
-    # )
+    dl_time = datetime.now() #figure we probably want now in this context?
+    collector = MRTCollector(
+        dl_time=dl_time,
+        cpus=cpu_count(),
+        base_dir=Path.home() / "mrt_data" / dl_time.strftime("%Y_%m_%d"),
+        limit_files_to
+    )
     # mrt_files = list(sorted(collector.run()))
     # mrt_files = [mrt_files[0]]
     # MHExportAnalyzer().run(mrt_files)
