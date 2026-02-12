b599a7bc738193e2213a2b30e61023a03ef9b6f4
Satchel Diaz <satchelrdiaz05@gmail.com>
2026-02-12 12:06:58 -0500

    Added a program arg to use single process

--- mrt_collector/__main__.py
+++ mrt_collector/__main__.py
@@ -13,14 +13,14 @@ def main():
     parser = argparse.ArgumentParser(prog='MRT Collector')
     #for use with running with limited files
     parser.add_argument('-lf', '--limit_files', type=int, help='Number of files to process; Leave blank for all')
-
+    parser.add_argument('-sp', '--single_process', action='store_true', help='Limits to single processining')
     args = parser.parse_args()
     limit_files_to = 0 if args.limit_files is None else args.limit_files
     # dl_time=datetime(2025, 3, 20, 0, 0, 0)
     dl_time = datetime.now() #figure we probably want now in this context?
     collector = MRTCollector(
         dl_time=dl_time,
-        cpus=cpu_count(),
+        cpus= 1 if args.single_process else cpu_count(),
         base_dir=Path.home() / "mrt_data" / dl_time.strftime("%Y_%m_%d"),
         limit_files_to=limit_files_to
     )
