40496a082ac172eee13c5002d0784edd4346c550
Satchel Diaz <satchelrdiaz05@gmail.com>
2026-03-07 14:46:48 -0500

    Testing parsing, having problems with permissions

--- mrt_collector/rib_dump_parse_funcs.py
+++ mrt_collector/rib_dump_parse_funcs.py
@@ -13,13 +13,6 @@ def bgpkit_parser(mrt_file: MRTFile) -> None:
     """Extracts info from raw dumps into parsed path"""
 
     if not mrt_file.parsed_path_psv.exists():
-        with mrt_file.parsed_path_psv.open("wb") as f:
-            subprocess.run(
-                ["bgpkit-parser", str(mrt_file.raw_path), "--psv"],
-                stdout=f
-            )
-
-        return
         check_call( # noqa
             f"bgpkit-parser {mrt_file.raw_path} --psv > {mrt_file.parsed_path_psv}",
             shell=True,
