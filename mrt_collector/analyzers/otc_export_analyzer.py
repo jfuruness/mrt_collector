89c1c95e678908521ada00f18efd5b7e68efeeff
Satchel Diaz <satchelrdiaz05@gmail.com>
2026-04-02 20:03:25 -0400

    OTC export analyzer, fixed an issue with communities export analyzer

--- mrt_collector/analyzers/otc_export_analyzer.py
+++ mrt_collector/analyzers/otc_export_analyzer.py
@@ -5,15 +5,15 @@ from .export_analyzer import ExportAnalyzer
 from .json_set_encoder import JSONSetEncoder
 
 
-class CommunitiesExportAnalyzer(ExportAnalyzer):
+class OTCExportAnalyzer(ExportAnalyzer):
     def __init__(
         self,
         base_dir: Path
     ) -> None:
 
         super().__init__(base_dir)
-        self.desc = "Extracting set of all BGP communities"
-        self.comm_data = set()
+        self.desc = "Extracting set of all OTC ASNs"
+        self.otc_data = set()
 
     def analyze(
         self,
@@ -21,19 +21,18 @@ class CommunitiesExportAnalyzer(ExportAnalyzer):
     ) -> None:
         """Collects full set of bgp communities"""
 
-        communities = row["communities"]
-        if communities == "":
+        otc = row["only_to_customer"]
+        if otc == "":
             return
 
-        communities = communities.split(" ")
-        self.comm_data.update(communities)
+        self.otc_data.add(otc)
 
     def dump_json(self) -> None:
         filepath = self.json_path
         filepath.parent.mkdir(parents=True, exist_ok=True)
         with open(filepath, "w") as f:
-            json.dump(self.comm_data, f, indent=4, cls=JSONSetEncoder)
+            json.dump(self.otc_data, f, indent=4, cls=JSONSetEncoder)
 
     @property
     def json_path(self) -> Path:
-        return self.base_dir / "analysis" / "communities.json"
+        return self.base_dir / "analysis" / "otc_asns.json"
